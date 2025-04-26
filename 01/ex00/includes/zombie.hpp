7201c3a4cbd3b084c69d418d9e412fe6a44ab45e
Enzo <emagnani@student.42.fr>
2025-04-26 15:35:46 +0200

    init cpp01

---
+++ 01/ex00/includes/zombie.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstdlib>
+#include <termios.h>
+#include <unistd.h>
+
+// colors
+#define GRN "\033[0;32m"
+#define RED "\033[0;31m"
+#define YEL "\033[0;33m"
+#define BLU "\033[0;34m"
+#define MAG "\033[0;35m"
+#define CYN "\033[0;36m"
+#define WHT "\033[0;37m"
+#define END "\033[0m"
+
+class Zombie {
+public:
+	Zombie(std::string name);
+	~Zombie();
+	void		announce( void );
+
+private:
+	std::string	_name;
+}
