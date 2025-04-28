ff26455ddeaf108dd921440ab01015d1290998c8
Enzo <kurutsukipv@gmail.com>
2025-04-28 20:06:11 +0200

    01/ ex04 pre-refactoring, somewhat working version

--- 01/ex04/srcs/main.cpp
+++ 01/ex04/srcs/main.cpp
@@ -12,10 +12,37 @@ int main(int argc, char **argv)
 		<< std::endl;
 		return(1);
 	}
-	(void)argc;
-	std::ofstream file;
-	file.open (argv[1]);
-	file << "aaaaaaa\n";
+	
+	std::string fileLine;
+	std::string filePath = argv[1];
+	std::string subFind = argv[2];
+	std::string subReplace = argv[3];
+
+	std::ifstream file(filePath.c_str());
+	if (!file.is_open())
+	{
+		std::cout << "could not open file " << filePath << std::endl;
+		return(1);
+	}
+	std::string appendFilename = filePath + ".replace";
+	std::ofstream destFile;
+	destFile.open(appendFilename.c_str());
+	if (!destFile.is_open())
+	{
+		std::cout << "could not open file " << filePath << std::endl;
+		return(1);
+	}
+	while (getline(file, fileLine))
+	{
+		size_t start = fileLine.find(subFind);
+		if (start != std::string::npos)
+		{
+			fileLine.erase(start, subFind.length());
+			fileLine.insert(start, subReplace);
+		}
+		destFile << fileLine << "\n";
+	}
 	file.close();
+	destFile.close();
 	return 0;
 }
