106c786021a2a529f51fbf71f6b862697f84e63c
Enzo <kurutsukipv@gmail.com>
2025-04-29 02:49:06 +0200

    01/ ex04 refactored and fixed

--- 01/ex04/srcs/main.cpp
+++ 01/ex04/srcs/main.cpp
@@ -2,47 +2,65 @@
 #include <string>
 #include <fstream>
 
+void replaceLine(const std::string& findStr, const std::string& replaceStr, std::string& currLine)
+{
+    if (findStr.empty())
+        return;
+    
+    size_t pos = 0;
+    while ((pos = currLine.find(findStr, pos)) != std::string::npos)
+    {
+        currLine.erase(pos, findStr.length());
+        currLine.insert(pos, replaceStr);
+        pos += replaceStr.length();
+    }
+}
 
+void processFile(const std::string& inputPath, const std::string& findStr, const std::string& replaceStr)
+{
+	std::ifstream inputFile(inputPath.c_str());
+	if (!inputFile.is_open())
+		throw std::runtime_error("Could not open input file: " + inputPath);
+	
+	std::string outputPath = inputPath + ".replace";
+	std::ofstream outputFile;
+
+	outputFile.open(outputPath.c_str());
+	if (!outputFile.is_open()) {
+		inputFile.close();
+		throw std::runtime_error("Could not create output file: " + inputPath);
+	}
+
+	std::string currLine;
+	while (getline(inputFile, currLine)) {
+		replaceLine(findStr, replaceStr, currLine);
+		outputFile << currLine << "\n";
+	}
+	outputFile.close();
+	inputFile.close();
+}
 
 int main(int argc, char **argv)
 {
 	if (argc != 4)
 	{
-		std::cout << "wrong number of arguments (argc = " << argc << ", should be 4)"
-		<< std::endl;
+		std::cerr	<< "Error: Incorrect number of arguments.\n" << "Usage: " << argv[0]
+					<< " <filename> <string_to_find> <string_to_replace>" << std::endl;
 		return(1);
 	}
 	
 	std::string fileLine;
 	std::string filePath = argv[1];
-	std::string subFind = argv[2];
-	std::string subReplace = argv[3];
+	std::string findStr = argv[2];
+	std::string replaceStr = argv[3];
+	
+	try {
+		processFile(filePath, findStr, replaceStr);
+	} catch (std::exception& e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+	} catch (...) {
+		std::cerr << "Unknown error" << std::endl;
+	}
 
-	std::ifstream file(filePath.c_str());
-	if (!file.is_open())
-	{
-		std::cout << "could not open file " << filePath << std::endl;
-		return(1);
-	}
-	std::string appendFilename = filePath + ".replace";
-	std::ofstream destFile;
-	destFile.open(appendFilename.c_str());
-	if (!destFile.is_open())
-	{
-		std::cout << "could not open file " << filePath << std::endl;
-		return(1);
-	}
-	while (getline(file, fileLine))
-	{
-		size_t start = fileLine.find(subFind);
-		if (start != std::string::npos)
-		{
-			fileLine.erase(start, subFind.length());
-			fileLine.insert(start, subReplace);
-		}
-		destFile << fileLine << "\n";
-	}
-	file.close();
-	destFile.close();
 	return 0;
 }
