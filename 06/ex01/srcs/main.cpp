9e5e5392c68bb7afbb8d05ab6142a756e28e6cdf
Enzo <kurutsukipv@gmail.com>
2025-05-27 03:59:30 +0200

    06/ex01 c tt bon

--- 06/ex01/srcs/main.cpp
+++ 06/ex01/srcs/main.cpp
@@ -1,9 +1,15 @@
 #include "Serializer.hpp"
 
+#define RESET		"\033[0m"
+#define CYAN		"\033[36m"
+#define YELLOW		"\033[33m"
+#define RED			"\033[31m"
+#define BOLD		"\033[1m"
+
 int	main(void)
 {
 
-	std::cout << "==== Serializer Test ====" << "\n"
+	std::cout << BOLD "==== Serializer Test ====" RESET << "\n"
 			  << "testing with a Data pointer,"
 			  << "containing the string \"Test Data\" "
 			  << "and the integer value 42\n" << std::endl;
@@ -18,29 +24,29 @@ int	main(void)
 		ptr = Serializer::serialize(data);
 		Data *tmp = Serializer::deserialize(ptr);
 
-		std::cout	<< "Data ptr: " << data << " -> name: " << data->name
+		std::cout	<< CYAN "Data ptr: " YELLOW << data << RESET << " -> name: " << data->name
 					<< ", integer value: " << data->value << std::endl;
 		
-		std::cout << "Serialized ptr(type = uintptr_t): " << ptr << std::endl;
+		std::cout << CYAN "Serialized ptr(type = uintptr_t): " YELLOW << ptr << RESET << std::endl;
 		
 		if (tmp)
-			std::cout	<< "Deserialized ptr(type = Data): " << tmp << " -> name: "
+			std::cout	<< CYAN "Deserialized ptr(type = Data): " YELLOW << tmp << RESET << " -> name: "
 						<< tmp->name << ", integer value: " << tmp->value << std::endl;
 		else
-			std::cout << "Deserialized ptr is NULL" << std::endl;
+			std::cout << RED "Deserialized ptr is NULL" RESET << std::endl;
 			
 	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
+		std::cout << RED << e.what() << RESET << std::endl;
 	}
 
 
-	std::cout << "\n==== Null Pointer Test ====" << std::endl;
+	std::cout << BOLD "\n==== Null Pointer Test ====" RESET << std::endl;
 
 	Data *nullData = 0;
 	try {
 		ptr = Serializer::serialize(nullData);
 	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
+		std::cout << RED <<e.what() << RESET << std::endl;
 	}
 	
 	delete data;
