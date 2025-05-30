2c6de3c23d3ce943a8e7d3f39002e3b234873616
kuruae <kurutsuipv@gmail.com>
2025-05-30 23:55:57 +0200

    08/ ex02 needs extensive testing and probably polishing

---
+++ 08/ex02/srcs/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#define RESET		"\033[0m"
+#define YELLOW		"\033[33m"
+#define BOLD		"\033[1m"
+#define RED			"\033[31m"
+#define GREEN		"\033[32m"
+#define CYAN		"\033[36m"
+
+# include "MutantStack.hpp"
+int main()
+{
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
+}
