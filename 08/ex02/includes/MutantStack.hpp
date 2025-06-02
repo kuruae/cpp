f296b05061ca0627c2b7d3bb569e66604c1905b0
kuruae <kurutsuipv@gmail.com>
2025-06-02 22:25:39 +0200

    08/ ex02 done? i think

--- 08/ex02/includes/MutantStack.hpp
+++ 08/ex02/includes/MutantStack.hpp
@@ -4,22 +4,22 @@
 #include <algorithm>
 #include <stack>
 
-template <typename T>
-class MutantStack : public std::stack<T>
+template <typename T, typename Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container>
 {
 public:
-	MutantStack() : std::stack<T>() {}
-	MutantStack(const MutantStack& other) : std::stack<T>(other) {}
+	MutantStack() : std::stack<T, Container>() {}
+	MutantStack(const MutantStack& other) : std::stack<T, Container>(other) {}
 	~MutantStack() {}
 
 	MutantStack& operator=(const MutantStack& other)
 	{
 		if (this != &other)
-			std::stack<T>::operator=(other);
+			std::stack<T, Container>::operator=(other);
 		return *this;
 	}
 
-	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T, Container>::container_type::iterator iterator;
 
 	iterator begin() {return this->c.begin(); }
 
